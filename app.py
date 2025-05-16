8301c2440c340cfd45f2d454999e84ac677ef8d1
Mudit Sharma <sharmamudit3112@gmail.com>
2025-05-16 20:00:58 +0530

    Fixing bugs

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file)
-    st.image(image, caption="Uploaded Image", use_container_width=True)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
 
     # Convert image to numpy array (EasyOCR input)
     img_array = np.array(image.convert('RGB'))
